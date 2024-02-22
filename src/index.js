b5094fd74280b10e605c438eeee714ff51b81ff9
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-02-22 16:51:32 +0000

    Added api functionality to my search engine
    The search engine now retrieves current temperature data relevant to the city input by the user and changes the placeholder temperature on the app to the correct degrees

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ function newCity(event) {
   let cityElement = document.querySelector("#search-form-input");
   let city = document.querySelector("#city");
   city.innerHTML = cityElement.value;
+
+  let apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement.value}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(newTemperature);
+}
+
+function newTemperature(response) {
+  let currentTemperature = document.querySelector(".weather-temperature");
+  let replacementTemperature = response.data.temperature.current;
+  currentTemperature.innerHTML = replacementTemperature;
 }
 
 let changeCity = document.querySelector("#search-form");
