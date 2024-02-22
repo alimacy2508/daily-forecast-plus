404b06353b03abc2c7746b707dba2126937db14b
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-02-22 17:07:42 +0000

    Increased the accuracy of my search engine results
    I increased the accuracy of my search engine input, so it would continue to output the correct display of a city name with a capital at the start, regardless of random spacing or capitialization

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 function newCity(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-
-  let cityCapital = cityElement.value.charAt(0).toUpperCase();
-  let newCity = cityCapital + cityElement.value.slice(1);
-  city.innerHTML = newCity;
 
   let apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement.value}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(newTemperature);
+  axios.get(apiUrl).then(newTemperatureCity);
 }
 
-function newTemperature(response) {
+function newTemperatureCity(response) {
   let currentTemperature = document.querySelector(".weather-temperature");
   let replacementTemperature = Math.round(response.data.temperature.current);
   currentTemperature.innerHTML = replacementTemperature;
+
+  let city = document.querySelector("#city");
+  let newCity = response.data.city;
+  city.innerHTML = newCity;
 }
 
 let changeCity = document.querySelector("#search-form");
